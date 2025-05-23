eb67fb1a257693c23dbacefa47241bdf668fdfe9
Natsil5 <37584575+Natsil5@users.noreply.github.com>
2025-05-23 17:46:28 +0000

    magie

--- module/rupture.js
+++ module/rupture.js
@@ -4,6 +4,7 @@ import LiberCharacterSheet from "./sheet/actor.js";
 import LiberCharacterData from "./data/actor.js";
 import LiberItemSheet from "./sheet/item.js";
 import LiberItemData from "./data/item.js";
+import LiberMagicSheet from "./sheet/magie.js";
 
 const ActorSheetV2 = foundry.applications.sheets.ActorSheetV2;
 const ItemSheetV2 = foundry.applications.sheets.ItemSheetV2;
@@ -28,9 +29,12 @@ Hooks.once("init", async function () {
   Actors.registerSheet("liber", LiberCharacterSheet, { types: ["character"], makeDefault: true });
   Items.unregisterSheet("core", ItemSheetV2);
   Items.registerSheet("liber", LiberItemSheet, {
-    types: ["item", "armor", "weapon", "magic"],
+    types: ["item", "armor", "weapon"],
     makeDefault: true
   });
-  
+    Items.registerSheet("liber", LiberMagicSheet, {
+    types: ["magic"],
+    makeDefault: true
+  });
 });
 
